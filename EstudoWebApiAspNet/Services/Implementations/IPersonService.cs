9a8f9e947507a2bbd09730914f18ac698358959c
Antonio-Zago <antoniozagodev@gmail.com>
2021-09-29 13:25:56 -0300

    Definindo interface IPersonService

---
+++ EstudoWebApiAspNet/Services/Implementations/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using EstudoWebApiAspNet.Models;
+using System.Collections.Generic;
+
+namespace EstudoWebApiAspNet.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person Update(Person person);
+        Person FindById(long id);
+        void Delete(long id);
+
+        List<Person> FindAll();
+    }
+}
